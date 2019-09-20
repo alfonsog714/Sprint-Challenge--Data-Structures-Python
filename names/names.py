1fc51a19303096e19509b03df7558e7ff28a7252
Alfonso <alfonso.garcia2324@gmail.com>
2019-09-20 09:24:14 -0700

    add instructions to names.py

--- names/names.py
+++ names/names.py
@@ -1,5 +1,15 @@
 import time
 
+"""
+Navigate into the names directory. Here you will find two text files containing 10,000 names each, along with a program names.py that compares the two files and prints out duplicate name entries. Try running the code with python3 names.py. Be patient because it might take a while: approximately six seconds on my laptop. What is the runtime complexity of this code?
+
+Six seconds is an eternity so you've been tasked with speeding up the code. Can you get the runtime to under a second? Under one hundredth of a second?
+
+You may NOT use the built in Python list or set for this problem
+
+(Hint: You might try importing a data structure you built during the week)
+"""
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -11,8 +21,10 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
+
+# O(n^2)
+for name_1 in names_1: # O(n) because the run complexity depends on the amount of names inputted
+    for name_2 in names_2: # O(n) because the run complexity depends on the amount of names inputted, same as above
         if name_1 == name_2:
             duplicates.append(name_1)
 
