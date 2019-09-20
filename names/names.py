dda46e746c25659adafb28d4dc34c195922ec23e
Alfonso <alfonso.garcia2324@gmail.com>
2019-09-20 09:50:16 -0700

    Passing names.py tests

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 """
 Navigate into the names directory. Here you will find two text files containing 10,000 names each, along with a program names.py that compares the two files and prints out duplicate name entries. Try running the code with python3 names.py. Be patient because it might take a while: approximately six seconds on my laptop. What is the runtime complexity of this code?
@@ -20,13 +21,23 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+
 duplicates = []
+# duplicates = set(names_1) & set(names_2)
+
+tree = BinarySearchTree(names_1[0])
+for name in names_1[1:]:
+    tree.insert(name)
+
+for name in names_2:
+    if tree.contains(name):
+        duplicates.append(name)
 
 # O(n^2)
-for name_1 in names_1: # O(n) because the run complexity depends on the amount of names inputted
-    for name_2 in names_2: # O(n) because the run complexity depends on the amount of names inputted, same as above
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1: # O(n) because the run complexity depends on the amount of names inputted
+#     for name_2 in names_2: # O(n) because the run complexity depends on the amount of names inputted, same as above
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
